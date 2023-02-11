a2b1a1e350558d7a2331c41a4a1192ce2ee53c0a
yuma <yuma.fuu05@gmail.com>
2023-02-12 05:44:34 +0900

    update

--- starry.rb
+++ starry.rb
@@ -5,7 +5,7 @@ class Starry < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ispec-inc/starry/releases/download/0.1.1/starry-x86_64-apple-darwin"
+      url "https://github.com/ispec-inc/starry/releases/download/v0.1.1/starry-x86_64-apple-darwin"
 
       def install
         bin.install "starry-x86_64-apple-darwin" => "starry"
@@ -13,7 +13,7 @@ class Starry < Formula
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/ispec-inc/starry/releases/download/0.1.1/starry-aarch64-apple-darwin"
+      url "https://github.com/ispec-inc/starry/releases/download/v0.1.1/starry-aarch64-apple-darwin"
 
       def install
         bin.install "starry-aarch64-apple-darwin" => "starry"
@@ -24,7 +24,7 @@ class Starry < Formula
 
   on_linux do
     t = "x86_64-unknown-linux-gnu"
-    url "https://github.com/ispec-inc/starry/releases/download/0.1.1/starry-x86_64-unknown-linux-gnu"
+    url "https://github.com/ispec-inc/starry/releases/download/v0.1.1/starry-x86_64-unknown-linux-gnu"
 
     def install
       bin.install "starry-x86_64-unknown-linux-gnu" => "starry"
